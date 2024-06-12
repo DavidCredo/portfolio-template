5b78884a3a2d85dde1299ad0de1d4003e9a5b5c6
InteractionEngineer <accounts@jona.email>
2024-06-12 11:33:39 +0200

    new structure for contents and dynamic global data like favicons and html descriptions

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
 import { projects } from "./projects/schema.ts";
 import { landingpage } from "./landingpage/schema.ts";
+import { layout} from "./layout/schema.ts";
 
 export const collections = {
     'projects': projects,
     'landingpage': landingpage,
+    'layout': layout,
 }
---
+++ src/content/layout/schema.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+export const layout = defineCollection({
+    type: "data",
+    schema: z.object({
+        head: z.object({
+            title: z.string(),
+            description: z.string(),
+            favicon: z.string(),
+        }),
+        header: z.object({
+            logo: z.string(),
+        }),
+        body: z.object({}).optional(),
+        footer: z.object({}).optional(),
+    }),
+});
