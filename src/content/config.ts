b8d7b1588f015c17b3880d05f19f47cb04998d35
InteractionEngineer <accounts@jona.email>
2024-06-14 19:53:44 +0200

    bugfixes after renaming imprint to personal

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { projects } from "./projects/schema.ts";
 import { landingpage } from "./landingpage/schema.ts";
 import { layout} from "./layout/schema.ts";
-import { imprint } from "./personal/schema.ts";
+import { personal } from "./personal/schema.ts";
 import { about } from "./about/schema.ts";
 
 export const collections = {
     'projects': projects,
     'landingpage': landingpage,
     'layout': layout,
-    'imprint': imprint,
+    'personal': personal,
     'about': about,
 }
