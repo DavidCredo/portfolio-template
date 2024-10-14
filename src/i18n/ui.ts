bf107234632ab6026f894b54dd18467566e99192
InteractionEngineer <accounts@jona.email>
2024-10-14 05:39:41 +0200

    BREAKING changes: I implemented a custom internationalization that works well with Astro's routing. Svelte-UI still needs to become dynamic. Language-switch needs attention.

---
+++ src/i18n/ui.ts
@@ -0,0 +1,71 @@
+export const languages = {
+    en: 'English',
+    de: 'Deutsch',
+};
+
+export const defaultLang = 'en';
+
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        'nav.projects': 'Portfolio',
+        'nav.about': 'About me',
+        'nav.cta': 'Text me!',
+        'nav.open': 'Open navigation',
+        'nav.close': 'Close navigation',
+
+        'footer.imprint': 'Imprint',
+        'footer.privacy': 'Privacy',
+
+        'imprint.title': 'Imprint',
+        'privacy.title': 'Privacy Policy',
+
+        'home.cta.primary': 'My work',
+        'home.cta.secondary': 'About me',
+
+        'about.title': 'About me',
+        'about.section.abstract': 'Who I am',
+        'about.section.skills': 'My skills',
+        'about.section.timeline': 'My milestones',
+
+        'projects.title': 'Projects',
+        'projects.subtitle': 'A selection of projects that I am particularly proud of.',
+        'projects.duration': 'Duration',
+        'projects.team': 'Team',
+        'projects.skills': 'Skills',
+
+        'srcButton.github': 'View on GitHub',
+        'srcButton.live': 'Visit live demo',
+        'srcButton.document': 'Load full document',
+    },
+    de: {
+        'nav.about': 'Über mich',
+        'nav.cta': 'Schreib mir!',
+        'nav.open': 'Navigation öffnen',
+        'nav.close': 'Navigation schließen',
+
+        'footer.imprint': 'Impressum',
+        'footer.privacy': 'Datenschutz',
+
+        'imprint.title': 'Impressum',
+        'privacy.title': 'Datenschutzerklärung',
+
+        'home.cta.primary': 'Meine Arbeiten',
+        'home.cta.secondary': 'Über mich',
+
+        'about.title': 'Über mich',
+        'about.section.abstract': 'Wer ich bin',
+        'about.section.skills': 'Meine Stärken',
+        'about.section.timeline': 'Meine Stationen',
+
+        'projects.title': 'Projekte',
+        'projects.subtitle': 'Eine Auswahl von Projekten, auf die ich besonders stolz bin.',
+        'projects.duration': 'Kapazität',
+        'projects.skills': 'Fähigkeiten',
+
+        'srcButton.github': 'Zum GitHub Repository',
+        'srcButton.live': 'Zur Live-Version',
+        'srcButton.document': 'Zum ausführlichen Dokument',
+    },
+} as const;
