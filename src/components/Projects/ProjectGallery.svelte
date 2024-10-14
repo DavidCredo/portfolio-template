ae951369399684b7ab8f03b7304e6129ada06900
InteractionEngineer <accounts@jona.email>
2024-10-14 14:41:03 +0200

    fixed routing for projects

--- src/components/Projects/ProjectGallery.svelte
+++ src/components/Projects/ProjectGallery.svelte
@@ -5,12 +5,27 @@
   import { selectedTagsStore } from "../../stores/selectedTagsStore";
   import { scale } from "svelte/transition";
   export let projects;
+  export let currentLang;
+  export let defaultLang;
 
-  $: filteredProjects = projects.filter((project) => {
-    if ($selectedTagsStore.length === 0) return true;
-    // Check if all selected tags are in the project
-    return $selectedTagsStore.every((tag) => project.data.tags.includes(tag))
-  }).sort((a, b) => a.data.id - b.data.id);
+  // retain routing consistency while language attributes are included in the project IDs
+  projects.map((project) => {
+    let pID = project.id.split("/")[1];
+    if (project.id.startsWith(`${defaultLang}/`)){
+      project.route = `/projects/${pID}`;
+    } else {
+      project.route = `/${currentLang}/projects/${pID}`;
+    }
+    return project;
+  });
+
+  $: filteredProjects = projects
+    .filter((project) => {
+      if ($selectedTagsStore.length === 0) return true;
+      // Check if all selected tags are in the project
+      return $selectedTagsStore.every((tag) => project.data.tags.includes(tag));
+    })
+    .sort((a, b) => a.data.id - b.data.id);
 </script>
 
 {#each filteredProjects as project (project.id)}
@@ -18,7 +33,7 @@
     <GlassCard
       classes="hover:bg-white/20 transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl active:scale-90 active:shadow-xl h-full max-w-lg"
     >
-      <a href={`/projects/${project.id}`} class="flex flex-col items-start">
+      <a href={project.route} class="flex flex-col items-start">
         <img
           src={project.data.thumbnail.src}
           alt={project.data.thumbnail.alt}
